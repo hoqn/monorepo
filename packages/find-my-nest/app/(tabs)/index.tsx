d4545b1a5fdffda4dcb03147d2cffa0d33bce3c0
Hogyun Jeon <akzm9999@gmail.com>
2025-12-20 18:48:58 +0900

    feat: 네이티브 탭

--- packages/find-my-nest/app/(tabs)/index.tsx
+++ packages/find-my-nest/app/(tabs)/index.tsx
@@ -18,11 +18,11 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">환영해요!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">첫 단계: 시도해보세요</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
