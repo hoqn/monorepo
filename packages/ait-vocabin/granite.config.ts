5bdb3ddb35edf782bd78c15385831dd0d44ef879
Hogyun Jeon <akzm9999@gmail.com>
2026-03-28 21:29:39 +0900

    chore:

--- packages/ait-vocabin/granite.config.ts
+++ packages/ait-vocabin/granite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from '@apps-in-toss/web-framework/config';
 
 export default defineConfig({
-  appName: 'vocabin', // TODO: 앱 이름 확정 후 변경 필요
+  appName: 'lingo-lock-de', // TODO: 앱 이름 확정 후 변경 필요
   brand: {
-    displayName: 'VocaBin',
-    primaryColor: '#3182F6',
+    displayName: 'Lingo Lock DE',
+    primaryColor: '#1E7AF2',
     icon: '',
   },
   web: {
